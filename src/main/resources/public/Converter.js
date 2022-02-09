e935c4a095ecb294f4f19eda552781d9b9caca99
DavidPZ666 <davidpz616@gmail.com>
2022-02-08 22:38:05 -0500

    Arreglos Despliege Heroku

--- src/main/resources/public/Converter.js
+++ src/main/resources/public/Converter.js
@@ -3,8 +3,9 @@ var converter=(function () {
 
     return {
         connection: function (name, ingresado) {
-            var url = 'http://localhost:4567/Converter/' + name + '/' + ingresado
-            fetch('http://localhost:4567/Converter/' + name + '/' + ingresado, {
+
+            var url = 'https://temperatureconverterarep.herokuapp.com/Converter/'+name+'/'+ingresado
+            fetch('https://temperatureconverterarep.herokuapp.com/Converter/'+name+'/'+ingresado, {
                 method: 'GET',
                 headers: {
                     "Content-type": "application/json"
